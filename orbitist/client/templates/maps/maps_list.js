f9e337894d31d69d3d9158656deb755b1ebebc10
ngunner <gunnernr@gmail.com>
2014-11-28 09:15:30 -0500

    added some sample static data

---
+++ orbitist/client/templates/maps/maps_list.js
@@ -0,0 +1,14 @@
+var mapsData = [
+  {
+    title: 'African Lions'
+  }, 
+  {
+    title: 'Meteor usage in New York'
+  }, 
+  {
+    title: 'Culture of Peru'
+  }
+];
+Template.mapsList.helpers({
+  maps: mapsData
+});
