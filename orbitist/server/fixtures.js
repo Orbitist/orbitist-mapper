391a3064a15bde618ad621805340d00f11174ccd
ngunner <gunnernr@gmail.com>
2014-11-28 10:31:04 -0500

    Added data to maps collection

---
+++ orbitist/server/fixtures.js
@@ -0,0 +1,13 @@
+if (Maps.find().count() === 0) {
+  Maps.insert({
+    title: 'African Lions'
+  });
+
+  Maps.insert({
+    title: 'Plastics in the Great Lakes'
+  });
+
+  Maps.insert({
+    title: 'Culture in Peru'
+  });
+}
