735b304382f4da8209e9ca04acf3b3f02d533c7b
ngunner <gunnernr@gmail.com>
2014-11-28 22:07:19 -0500

    Removed insecure, and allowed certain writes

--- orbitist/client/templates/maps/map_create.js
+++ orbitist/client/templates/maps/map_create.js
@@ -2,11 +2,11 @@ Template.mapCreate.events({
   'submit form': function(e) {
     e.preventDefault();
 
-    var post = {
+    var map = {
       title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Maps.insert(map);
+    map._id = Maps.insert(map);
     Router.go('mapPage', map);
   }
 });
--- orbitist/lib/collections/maps.js
+++ orbitist/lib/collections/maps.js
@@ -1,1 +1,8 @@
 Maps = new Mongo.Collection('maps');
+
+Maps.allow({
+  insert: function(userId, doc) {
+    // only allow posting if you are logged in
+    return !! userId;
+  }
+});
