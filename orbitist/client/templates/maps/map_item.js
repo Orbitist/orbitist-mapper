9551a0b6bea74be222c5516a05645a73dcd425c4
ngunner <gunnernr@gmail.com>
2014-12-03 07:33:19 -0500

    Dropped story count

--- orbitist/client/templates/maps/map_item.js
+++ orbitist/client/templates/maps/map_item.js
@@ -1,8 +1,5 @@
 Template.mapItem.helpers({
   ownMap: function() {
     return this.userId === Meteor.userId();
-  },
-  storiesCount: function() {
-    return Stories.find({mapId: this._id}).count();
   }
 });
